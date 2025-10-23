170ca5178a594603ee1a175833292909c8f26b13
white <ionicalarisa797@gmail.com>
2025-10-23 15:35:15 +0300

    working chat sketch

--- backend/src/test/java/org/example/marketplace/MarketplaceApplicationTests.java
+++ backend/src/test/java/org/example/marketplace/MarketplaceApplicationTests.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MarketplaceApplicationTests {
+class M0arketplaceApplicationTests {
+
 
     @Test
     void contextLoads() {
