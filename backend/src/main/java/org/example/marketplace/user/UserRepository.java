0cf877adc159faef3995670c443c264c0843fb7d
Andreea <andreeadura6@gmail.com>
2025-12-04 15:57:13 +0200

    frontend

--- backend/src/main/java/org/example/marketplace/user/UserRepository.java
+++ backend/src/main/java/org/example/marketplace/user/UserRepository.java
@@ -10,6 +10,4 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByEmailIgnoreCase(String email);
     boolean existsByEmailIgnoreCase(String email);
 
-
-
 }
